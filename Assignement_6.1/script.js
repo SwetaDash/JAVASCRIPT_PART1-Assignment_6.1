fd861c2651f093389fe9448ba3f46178128c2dfd
SwetaDash <43117650+SwetaDash@users.noreply.github.com>
2018-12-01 12:17:22 +0530

    Add files via upload

---
+++ Assignement_6.1/script.js
@@ -0,0 +1,10 @@
+//for alert in head section
+var headv=document.querySelector('head').innerHTML;
+alert('I am from <head>');
+
+//for alert in body section
+var bodyv=document.querySelector('body').innerHTML;
+alert('I am from <body>');
+
+//for alert in JS
+alert("I am from external file");
